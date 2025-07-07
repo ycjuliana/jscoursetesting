662a14c524d690b9a047cba934314abeee330241
ycjuliana <ycjuliana@gmail.com>
2025-07-07 12:39:26 -0400

    message

---
+++ Scope/scope_lab.js
@@ -0,0 +1,23 @@
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+{
+// Block scope
+// var blockVar = "I'm a block-scoped var";
+// let blockLet = "I'm a block-scoped let";
+const blockConst = "I'm a block-scoped const";
+}
+
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+    }
+    show();
+    
+    console.log(functionVar); // Throws ReferenceError
+    console.log(functionLet); // Throws ReferenceError
+    console.log(functionConst); // Throws ReferenceError
